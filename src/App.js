32a1e59be6ba6ebfdcad5a0f2cff04b7b20eac73
sam000urai <sam000urai@gmail.com>
2023-02-11 15:36:59 +0900

    add

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React
+          <butan>Hello World</butan>
         </p>
       </header>
     </div>
