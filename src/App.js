05c9ec5402740b7d9e8e8cbaad7c41d033a3245a
sam000urai <sam000urai@gmail.com>
2023-02-11 20:19:14 +0900

    add-EXTRA-2

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ const App = () => {
     setCount(count - 1);
   };
 
+  const [names, setNames] = useState([]);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setNames([...names, "satake"]);
+  };
+
   return (
     <div>
       <button onClick={increment}>いいね！</button>
@@ -20,6 +27,11 @@ const App = () => {
 
       <button onClick={decrement}>よくないね！</button>
       <p>{count}</p>
+
+      <button onClick={() => setCount(0)}>Reset</button>
+
+      <p>{names}</p>
+      <button onClick={handleAdd}>Add</button>
     </div>
   );
 }
