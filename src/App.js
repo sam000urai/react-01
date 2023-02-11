e02a81a15bfdfbdda85500911a6f47dc58b121a0
sam000urai <sam000urai@gmail.com>
2023-02-11 19:01:27 +0900

    add-よくないね！

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
-function App() {
-  const handleClick = () => {
-    window.alert("Hello World");
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    console.log(count);
   };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         <button onClick={handleClick}>Hello World</button>
-        </p>
-      </header>
+    <div>
+      <button onClick={increment}>いいね！</button>
+      <p>{count}</p>
+
+      <button onClick={decrement}>よくないね！</button>
+      <p>{count}</p>
     </div>
-    
   );
-} 
+}
+
 export default App;
