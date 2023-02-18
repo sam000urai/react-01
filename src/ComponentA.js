2d548ad0e57038ef42389f29fe5f782ac3ac9cf0
sam000urai <sam000urai@gmail.com>
2023-02-18 14:18:36 +0900

    add : normal2

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 import React from 'react';
 import { Link } from 'react-router-dom';
 const ComponentA = () => {
@@ -23,6 +24,20 @@ const ComponentA = () => {
 
                 </p>
             </div >
+            <div>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                </Card>
+            </div>
         </div >
     );
 };
