3e5a2d30d665b68b22ce6f76efef782b81ee2923
sam000urai <sam000urai@gmail.com>
2023-02-19 16:23:39 +0900

    add : extra-1

--- src/components/ComponentC.js
+++ src/components/ComponentC.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-const ComponentC = () => {
+import { Button, Table } from 'react-bootstrap';
+
+function ComponentC() {
+    const [comments, setComments] = useState([]);
+
+    const handleClick = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+        const data = await response.json();
+        setComments(data);
+    };
+
     return (
         <div>
-            <div>ComponentC</div>
-            <Link to="/">ComponentAへ移動</Link>
-        </div >
+            <div>
+                <Link to="/">ComponentAへ移動</Link>
+            </div>
+            <div>
+                <Link to="/components/componentb">ComponentBへ移動</Link>
+            </div>
+            <Button onClick={handleClick}>Get Data</Button>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>postId</th>
+                        <th>id</th>
+                        <th>name</th>
+                        <th>email</th>
+                        <th>body</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {comments.map(comment => (
+                        <tr key={comment.id}>
+                            <td>{comment.postId}</td>
+                            <td>{comment.id}</td>
+                            <td>{comment.name}</td>
+                            <td>{comment.email}</td>
+                            <td>{comment.body}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </div>
     );
-};
+}
+
 export default ComponentC;
